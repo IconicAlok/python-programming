071fb7c1e2a9b7adeacccd124200cd494c347dd5
alokkuri <alokkuri98@gmail.com>
2026-02-13 19:35:02 +0600

    Typecasting different data types

--- App.py
+++ App.py
@@ -1,51 +1,21 @@
-# Variable is a container for value (string, integer, float, boolean)
-#           a variable behaves as if it was the vaulue it contains
+# Typecasting = The  process of converting a variable from one datatype to another
+#                 str(), int(), float(), bool()
 
-#Strings
-first_name = "Alok"
-food = "pizza"
-email = "alok27@yahoo.com"
-
-# Integers
+name = ""
 age = 25
-quantity = 3
-num_of_student = 30
-
-# Float
-price = 10.99
 gpa = 3.2
-distance = 5.5
+is_student = True
 
-# Boolean
-is_student = False
-for_sale = False
-is_online = False
+gpa = int(gpa)
 
-print(f"Hello {first_name}")
-print(f"You like {food}")
-print(f"Your email is {email}")
+age = float(age)
 
-print(f"You are {age} years old")
-print(f"You are buying {quantity} items")
-print(f"Your class has {num_of_student} students")
+age = str(age)
+# print(type(age))
 
-print(f"The price is ${price}")
-print(f"Your gpa is: {gpa}")
-print(f"You ran {distance} km")
+# age+=1 #error 
+# age+="1" #concatenate 
 
-print(f"Are you student?: {is_student}")
+name = bool(name)
 
-if is_student:
-    print("You are a student")
-else:
-    print("You are NOT a student")
-
-if for_sale:
-    print("That item for sale")
-else:
-    print("That item is NOT available")
-
-if is_online:
-    print("You are online")
-else:
-    print("You are offline")
+print(name)
