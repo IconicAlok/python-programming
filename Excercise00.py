e2d354155c2e1f6d63703b977924d47077b249cb
alokkuri <alokkuri98@gmail.com>
2026-02-14 12:52:58 +0600

    Calculator Program

---
+++ Excercise00.py
@@ -0,0 +1,23 @@
+# Python calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 =  float(input("Enter the 1st number: "))
+
+if(operator == "+"):
+    result = num1 + num2
+    print(f"Result = {round(result, 3)}")
+
+elif(operator == "-"):
+    result = num1 - num2
+    print(f"Result = {round(result, 3)}")
+
+elif(operator == "*"):
+    result = num1 * num2
+    print(f"Result = {round(result, 3)}")
+
+elif(operator == "/"):
+    result = num1 / num2
+    print(f"Result = {round(result, 3)}")
+else:
+    print(f"{operator} is not valid")
