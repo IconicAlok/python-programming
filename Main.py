4c3389dee215f0ae5cced77ba3162f3a26c18272
alokkuri <alokkuri98@gmail.com>
2026-02-18 11:31:44 +0600

    Shopping cart program

--- Main.py
+++ Main.py
@@ -1,82 +1,26 @@
-# collection = single "variable" used to store multiple values
-#   List = [] ordered and changeable. Duplicate OK
-#   Set = {} unordered and immutable, but Add/ Remove ok. N0 duplicates
-# Tuple = () ordered and unchangeable, Duplicate OK, Faster
+# Shopping cart program
 
-# fruits = ["apple","orange","banana","coconut"]
+foods  = []
+prices = []
+total = 0
 
-# print(dir(fruits))
-# print(help(fruits))
-# print(len(fruits))
-# print("pineapple" in fruits)
+while True:
+    food = input("Enter a food you like to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
 
-# fruits[0] = "pineapple"
-# fruits.append("pineapple")
-# fruits.remove("apple")
-# fruits.insert(0,"pineapple")
-# fruits.sort()
-# fruits.reverse()
-# fruits.clear()
+print("----- Your Cart -----")
 
-# print(fruits.index("apple"))
-# print(fruits.index("coconut"))
-# print(fruits.index("pineapple")) #error - cause not in the list
+for food in foods:
+    print(food, end=" ")
 
-# print(fruits.count("banana"))
-# print(fruits.count("pineapple"))
+print()
 
+for price in prices:
+    total+=price
 
-# print(fruits)
-
-# for fruit in fruits:
-#     print(fruit,end=" ")
-
-
-
-"""
-print(fruits)
-
-print(fruits[0])
-print(fruits[1])
-print(fruits[2])
-print(fruits[3])
-
-print(fruits[0:3])
-print(fruits[:3])
-print(fruits[::2])
-print(fruits[::-1])
-
-for fruit in fruits:
-    print(fruit,end=" ")
-"""
-# set = {} unordered and immutable, but Add/ Remove ok. N0 duplicates
-
-# fruits = {"apple", "orange", "banana", "coconut" }
-# print(dir(fruits))
-# print(help(fruits))
-# print(len(fruits))
-# print("pineapple" in fruits)
-# print(fruits[0]) # error 'set' object is not subscriptable
-
-# fruits.add("pineapple")
-# fruits.remove("apple")
-# fruits.pop()
-# fruits.clear()
-# print(fruits)
-
-
-
-# tuples = () ordered and unchangeable, Duplicate OK, Faster
-
-fruits = ("apple","orange","banana","coconut","coconut")
-# print(dir(fruits))
-# print(help(fruits))
-# print(len(fruits))
-# print("pineapple" in fruits)
-
-# print(fruits.index("apple"))
-print(fruits.count("coconut"))
-
-# print(fruits)
-for fruit in fruits:
-    print(fruit,end=" ")
+print(f"Your total is : ${total}")
