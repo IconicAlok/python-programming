9d6918d73d944017b8c2d1aed1af784d1ea89ba0
alokkuri <alokkuri98@gmail.com>
2026-02-20 18:36:32 +0600

    Default arguments

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
-# Functions = A block of reusable code
-#               place () after the function name invoke it.
+# default arguments = A default value for certain parameter
+#                     default is used when that argument is omitted
+#                     make your function more flexible, reduce # of argument
+#                     1. Positional 2. DEFAULT 3. Arbitrary
 
-def happy_birthday(name, age):
-    print(f"Happy birthday {name}!")
-    print(f"You are {age} years old!")
-    print("Happy birthday to you!")
-    print()
+def net_price(list_price, discount = 0.0, tax = 0.05):
+    return list_price * (1 - discount) * (1 + tax)
 
-
-happy_birthday("alok", 20)
-happy_birthday("kuri", 30)
-happy_birthday("Tom", 40)
+# print(net_price(500,0,0.05))
+# print(net_price(500))
+# print(net_price(500, 0.1))
+print(net_price(500, 0.1, 0.0))
--- example3.py
+++
@@ -1,9 +0,0 @@
-
-def create_name(first, last):
-    first = first.capitalize()
-    last = last.capitalize()
-    return first+" "+last
-
-# full_name = create_name("alok", "kuri")
-full_name = create_name("quenton","decock")
-print(full_name)
