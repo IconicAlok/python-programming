a3c0e1c78783668be904581b52197442785c9698
alokkuri <alokkuri98@gmail.com>
2026-02-21 08:27:46 +0600

    Iterables

--- Main.py
+++ Main.py
@@ -1,19 +1,43 @@
-# *args         = allow you to pass multiple non-key arguments
-# **kwargs      = allow you to pass multiple keyword-arguments
-#                 1. Positional 2. default 3. keyword 4. ARBITRARY
+# Iterables = An object/collection that can be return its element one at a time,
+#             Allowing it to be iterated over in a loop
 
-# def add(a, b):
-#     return a + b
 
-# print(add(1,2, 3)) # error add() takes 2 positional arguments but 3 were given
+# numbers = [1, 2, 3, 4, 5]
+numbers = (1,2,3,4,5)
+fruits = {"Apple", "Orange", "Banana", "Coconut"}
 
-def add(*args):
-    # print(type(args)) # tuples
-    total = 0
-    for arg in args:
-        total += arg
-    return total
+# the name of the current element of out iterable should be
+# descriptive for what iterating over
+# for number in reversed(numbers):
+#     print(number, end=" - ")
 
-print(add(1,2,3))
-print(add(1,2,3,4))
-print(add(1))
+for num in numbers:
+    print(num)
+
+
+for fruit in fruits :
+    print(fruit)
+
+
+# for fruit in reversed(fruits):
+#     print(fruit)
+#     TypeError: 'set' object is not reversible
+
+name = "Alok Kuri"
+
+for character in name:
+    print(character, end=" ")
+
+print()
+
+
+my_dictionary = {"A":1, "B":2, "C":3}
+
+for key in my_dictionary:
+    print(key)
+
+for value in my_dictionary.values():
+    print(value)
+
+for key, value in my_dictionary.items():
+    print(f"{key} : {value}")
