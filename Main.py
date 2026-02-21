ab612b0f89dc6432fe74093035d20281c23a985e
alokkuri <alokkuri98@gmail.com>
2026-02-21 11:46:22 +0600

    Match case statement

--- Main.py
+++ Main.py
@@ -1,29 +1,15 @@
-# list comprehension = A concise way to create lists in python
-#                      Compact and easier to read and traditional loops
-#                      [expression for value in iterable if condition]
-
-# doubles = []
-# for x in range(1,11):
-#     doubles.append(x * 2)
-#
-# print(doubles)
-
-doubles = [x * 2 for x in range(1,11)]
-triple = [y * 3 for y in range(1,11)]
-square = [z * z for z in range(1,11)]
-
-fruits = [fruit.upper() for fruit in ["apple", "orange", "banana", "coconut"]]
-fruits_char = [fruit[0] for fruit in ["apple", "orange", "banana", "coconut"]]
-
-numbers = [1, -2, 3, -4, 5, -6, -7]
-positive_num = [num for num in numbers if num>=0]
-negative_num = [num for num in numbers if num<0]
-even_num = [num for num in numbers if num % 2==0]
-odd_num = [num for num in numbers if num % 2 == 1]
-# print(odd_num)
+# match case statement (switch) = An alternative to using many elif statements.
+#                                   Execute some code if a value matches a 'case'.
+#                                   Benefits: cleaner and syntax more readable
 
 
-grades = [85, 42, 79, 90, 56, 61, 30]
-passing_grade = [grade for grade in grades if grade >= 60]
+def is_weekend(day):
+    match day:
+        case "Sunday"| "Saturday":
+            return True
+        case "Monday"| "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
+        case _:
+            return False
 
-print(passing_grade)
+print(is_weekend("Saturday"))
